b424a1b48d6eb2f6ed4d3344ef1a6f8b8b4a9216
Benjamin Wong <44148480+chiyeon@users.noreply.github.com>
2021-04-24 10:52:45 -0700

    finished basic fishing mechanic + clicking minigame

--- 48/logic/Caster.gd
+++ 48/logic/Caster.gd
@@ -9,9 +9,14 @@ onready var animPlayer = $"boat-model/model/AnimationPlayer"
 var fishHint = preload("res://scenes/FishHint.tscn");
 var activeFishes = [];						# list of all active fish hints
 var fishBobberMaxRange = 1;				# max range of a bobber being considered near a fish
+var fishBobberMaxRangeHeight = 2;
 var bobberNearActiveFish = false;		# is bobber in radius of fish hint
 #var fishNearBobberID: int;				# id of the fish that is near the bobber
 onready var catchTimer = $"catch-timer";	# used to time the capture of fish
+var isCatchingFish = false;				# if player is in act of reeling in;
+var isInMinigame = false;
+var clickCounter = 0;
+var clickGoal = 0;
 
 # bobber instance
 var bobber = preload("res://scenes/Bobber.tscn");
@@ -50,25 +55,47 @@ func _physics_process(delta):
 		model.rotation.y = lerp_angle(model.rotation.y, -angle + offset, 0.15);
 
 func _process(delta):
-	if Input.is_action_just_pressed("Throw") and !hasThrownLine:
-		animPlayer.play("Cast");
-	elif Input.is_action_just_pressed("Throw") and hasThrownLine and !isBobberLocked:
-		LockBobber();
-	elif Input.is_action_just_pressed("Throw") and hasThrownLine and isBobberLocked:
-		CatchLine();
-	
-	# increase line distance after throwing, interpolate the bobber to the water spot
-	if(hasThrownLine):
-		# make hook go deeper at specified rate until max if bobber ISNT locked
-		if(!isBobberLocked):
-			if(lineDistance < maxDepth):
-				lineDistance += delta * lineDropSpeed;
-			else:
-				lineDistance = maxDepth;
-				LockBobber();
+
+	if isInMinigame:
+		clickGoal = 25;
+
+		if(pow(-1, clickCounter) == 1):
+			if Input.is_action_just_pressed("Throw"):
+				clickCounter += 1;
+				print(clickGoal - clickCounter + 1, " left");
+		else:
+			if Input.is_action_just_pressed("Reel"):
+				clickCounter += 1;
+				print(clickGoal - clickCounter + 1, " left");
+
+		if(clickCounter >= clickGoal):
+			CatchFish();
+	else:
+		if Input.is_action_just_pressed("Throw") and !hasThrownLine:	# click to throw line
+			animPlayer.play("Cast");
+		elif Input.is_action_just_pressed("Throw") and hasThrownLine and !isBobberLocked:	# click to lock bobber
+			LockBobber();
+		elif Input.is_action_just_pressed("Throw") and hasThrownLine and isBobberLocked and isCatchingFish:	# if fish bit, click to catch
+			isInMinigame = true;
+			print("started catching minigame click!");
+		elif Input.is_action_just_pressed("Throw") and hasThrownLine and isBobberLocked and !isCatchingFish and !bobberNearActiveFish: # reel in
+			CatchLine();
+
+		if Input.is_action_just_pressed("Reel"):
+			CatchLine();
 		
-		# visually move bobber to water
-		bobberInstance.global_transform.origin = bobberInstance.global_transform.origin.linear_interpolate(bobberInstancePosition, 0.1);
+		# increase line distance after throwing, interpolate the bobber to the water spot
+		if(hasThrownLine):
+			# make hook go deeper at specified rate until max if bobber ISNT locked
+			if(!isBobberLocked):
+				if(lineDistance < maxDepth):
+					lineDistance += delta * lineDropSpeed;
+				else:
+					lineDistance = maxDepth;
+					LockBobber();
+			
+			# visually move bobber to water
+			bobberInstance.global_transform.origin = bobberInstance.global_transform.origin.linear_interpolate(bobberInstancePosition, 0.1);
 
 func ThrowLine():
 	# reset line distance
@@ -84,9 +111,6 @@ func ThrowLine():
 	var spaceState = get_world().get_direct_space_state();
 	var mouseWorldPos = spaceState.intersect_ray(from, to).position;
 	bobberInstancePosition = Vector3(mouseWorldPos.x, 0, mouseWorldPos.z);
-
-	# check if player is throwing near a fish
-	CheckBobberCloseToFish(bobberInstancePosition);
 	
 	# spawn bobber instance
 	bobberInstance = bobber.instance();
@@ -108,6 +132,15 @@ func CatchLine():
 		# turn off catch timer
 		catchTimer.stop();
 
+		# not catching anymore
+		isCatchingFish = false;
+
+		# click count
+		clickCounter = 0;
+
+		# no minigame
+		isInMinigame = false;
+
 		# catch, delete bobber
 		hasThrownLine = false;
 		bobberInstance.queue_free();
@@ -119,6 +152,9 @@ func CatchLine():
 func LockBobber():
 	isBobberLocked = true;
 	print("Bobber locked at a depth of ", lineDistance);
+
+	# check if player is throwing near a fish
+	CheckBobberCloseToFish(bobberInstancePosition);
 
 func AddFish(rarity, location):
 	# create instance of bubbling hint
@@ -155,16 +191,24 @@ func CheckBobberCloseToFish(point):
 	for fish in activeFishes:
 		var adjustedFishLocation = Vector3(fish.location.x, 0, fish.location.z);
 		var dist = point.distance_to(adjustedFishLocation);
-		if(dist < fishBobberMaxRange):
+
+		# if is close enough inc height
+		if(dist < fishBobberMaxRange and fish.location.y + lineDistance < fishBobberMaxRangeHeight):
 			# set vars
 			bobberNearActiveFish = true;
 			var fishID = activeFishes.find(fish, 0);
 			print("near (", dist ,") a fish ID of ", fishID);
 
 			# set timer
-			catchTimer.wait_time = rng.randf_range(1, 2) * fish.rarity;
+			catchTimer.wait_time = rng.randf_range(1.5, 3) * (fish.rarity + 1);
+			catchTimer.connect("timeout", self, "BiteLine", [fishID]);
 			catchTimer.start();
 
-func CatchFish(fishID):
-	print("Caught fish ", fishID, "!");
+func BiteLine(fishID):
+	print("Fish ", fishID, " bit the line !");
+	isCatchingFish = true;
 
+func CatchFish():
+	# reel in line
+	CatchLine();
+	print("caught fish!");
